79434a06cdf8787197b56399808d3d70cc7b54ef
RiteshSingh114 <riteshkrsingh114@gmail.com>
2022-01-26 21:59:25 +0530

    Added Model Validations

--- BookStoreAPI/WebApplication1/Models/BookModel.cs
+++ BookStoreAPI/WebApplication1/Models/BookModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace BookStore.Models
     public class BookModel
     {
         public int id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
     }
 }
